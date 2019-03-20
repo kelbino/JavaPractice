2a758cfb6e1206cda0a883ce58d354ae71f3483c
Freed <Freed@192.168.0.12>
2019-03-20 19:43:33 -0400

    Additional work

---
+++ Program Challenge 1/src/info.java
@@ -0,0 +1,19 @@
+
+public class info {//class declaration
+	
+	public static void main(String[]args) {//method declaration
+		
+		String name;
+		int age;
+		double annualPay;
+		
+		name = "Kelvin Salvodon";
+		age = 27;
+		annualPay = 80000.00;
+		
+		System.out.println("My Name is " + name + ", my age is " + age + " and I hope to earn $" + annualPay + " per year.");
+		
+		
+	}
+
+}
