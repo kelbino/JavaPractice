4feeb83932fda84c26a777a25da022652c162ae5
Freed <Freed@192.168.0.12>
2019-03-20 19:42:50 -0400

    Additional Work

---
+++ Chapter1PC3/src/PersonalInfo.java
@@ -0,0 +1,28 @@
+
+public class PersonalInfo {//class declaration
+	
+	public static void main(String[]args) {//main method declaration 
+		
+		//enter statements
+		
+		String name, city, state, street, phoneNum, major;
+		int addressNum, ZIP, areaCode;
+		
+		name = "Kelvin Salvodon";
+		city = "Brooklyn";
+		state = "New York";
+		street = "Robardo Street";
+		phoneNum = "426-4019";
+		major = "Computer Programming & Information Systems";
+		
+		addressNum = 14325;
+		ZIP = 11080;
+		areaCode = 347;
+		
+		System.out.println("Name: " + name + "\nAddress: " + addressNum + " " + street + "\nCity: " + city + 
+				"\nZip Code: " + ZIP + "\nState: " + state + "\nPhone Number: " + "(" + areaCode + ")-" + phoneNum + "\nCollege Major: " + major);		
+		
+		
+	}
+
+}
