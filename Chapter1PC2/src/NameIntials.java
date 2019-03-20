d3e7167a4f1c8d743bfb735177908e2593513f52
Freed <Freed@192.168.0.12>
2019-03-20 19:39:34 -0400

    Basic Practice 1st of many

---
+++ Chapter1PC2/src/NameIntials.java
@@ -0,0 +1,25 @@
+
+public class NameIntials {//class declaration
+	
+	public static void main(String[] args) {//method declaration 
+		
+		//enter statements
+		
+		String firstName, middleName, lastName;
+		
+		firstName = "Kelvin";
+		middleName = "Herby";
+		lastName = "Salvodon";
+		
+		char firstInitial, middleInitial, lastInitial;
+		firstInitial = 'K';
+		middleInitial = 'H';
+		lastInitial = 'S';
+		
+		System.out.println("My name is " + firstName + " " + middleName + " " + lastName + ".");
+		System.out.println("My Initials are " + firstInitial + " . " + middleInitial + " . " + lastInitial);
+		
+		
+	}
+
+}
